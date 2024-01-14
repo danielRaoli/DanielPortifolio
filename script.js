6c9dda37b8678fcf5a949e45dd6d8147e58d3882
Daniel Raoli <danielraoli074@gmail.com>
2024-01-14 11:22:38 -0300

    about section skills

--- script.js
+++ script.js
@@ -37,6 +37,46 @@
     }
  ]
 
+ const dataskills = [
+    {
+        name:"jsimage",
+        alt: "ícone js"
+    },
+    {
+        name: "cssimage",
+        alt: "ícone css"
+    },
+    {
+        name: "html_1051277",
+        alt: "ícone html"
+    },
+    {
+        name:"tsimage",
+        alt:"ícone type script"
+    },
+    {
+        name: "c-sharp_6132221",
+        alt: "ícone c#"
+    },
+    {
+        name: "netimage",
+        alt: "ícone .Net"
+    },
+    {
+        name: "sqlserverimage",
+        alt: "ícone sql server"
+    },
+    {
+        name: "dockerimage",
+        alt: "ícone docker"
+    },
+    {
+        name: "gitimag ",
+        alt: "ícone git"
+    }
+
+ ]
+
  const wrappercards = document.querySelector(".wrapper_projects");
 
  dataProjects.forEach((p) => {
@@ -55,3 +95,60 @@
  </article>`)
  });
 
+
+const btnAbout = document.querySelector('#btn-about');
+const btnTec = document.querySelector('#btn-tec');
+const aboutArea = document.querySelector('#about-area')
+
+let tecActivate = false;
+let aboutActivate = true;
+
+btnTec.addEventListener('click',() =>{
+
+    if(tecActivate){
+        return;
+    }
+
+    aboutArea.removeChild(aboutArea.firstChild);
+    const wraperSkills = document.createElement('div');
+    wraperSkills.classList.add('wrapper_skill');
+
+    dataskills.forEach((skill) => {
+        wraperSkills.insertAdjacentHTML('beforeend', `
+    <div class="skill">
+        <img src="Assets/${skill.name}.png" alt="${skill.alt}">
+    </div>`)
+    aboutArea.insertAdjacentElement('afterbegin', wraperSkills);
+    const skills = document.querySelectorAll('.skill');
+    skills.forEach((skill) => {
+        setTimeout(() => {
+            skill.classList.add('-activate')
+        },100)
+    })
+    tecActivate = true;
+    aboutActivate = false;
+
+    });
+} );
+
+
+ btnAbout.addEventListener('click',() => {
+    if(aboutActivate){
+        return;
+    }
+    aboutArea.removeChild(aboutArea.firstChild);
+
+    aboutArea.insertAdjacentHTML('afterbegin',`<p class="description">
+    Olá me chamo Daniel Raoli, graduado em análise e desenvolvimento de sistemas curso que me deu oportunidade de ter contato acadêmico com diversos nichos, linguagens e ensinamentos relacionados a software e seu desenvolvimento. Atualmente me encontro cursando pós graduação em Desenvolvimento Web Full Stack área que acabei me apaixonando ainda mais durante estudos e projetos pessoais. Já atuo como freelancer sendo desenvolvedor front-end criando sites estáticos com Css e Html, JavaScript, React e tenho experiência no desenvolvimento back-end de sites dinâmicos utilizando C# .Net . 
+     </p>`);
+
+aboutActivate = true;
+tecActivate = false;
+
+});
+
+if(aboutActivate){
+    aboutArea.insertAdjacentHTML('afterbegin',`<p class="description">
+    Olá me chamo Daniel Raoli, graduado em análise e desenvolvimento de sistemas curso que me deu oportunidade de ter contato acadêmico com diversos nichos, linguagens e ensinamentos relacionados a software e seu desenvolvimento. Atualmente me encontro cursando pós graduação em Desenvolvimento Web Full Stack área que acabei me apaixonando ainda mais durante estudos e projetos pessoais. Já atuo como freelancer sendo desenvolvedor front-end criando sites estáticos com Css e Html, JavaScript, React e tenho experiência no desenvolvimento back-end de sites dinâmicos utilizando C# .Net . 
+            </p>`)
+}
