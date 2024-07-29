ca7d974f3cda2e5a98421e54f6cb00f50fce03e0
danielRaoli <danielraoli074@gmail.com>
2024-07-29 13:17:33 -0300

    feat: add projects

--- script.js
+++ script.js
@@ -1,45 +1,76 @@
- const dataProjects = [
+const dataProjects = [
     {
-        link:"LR-Contabilidade",
-        image:"contabilidade",
+        link: "LR-Contabilidade",
+        image: "contabilidade",
         alt: "Site de contabilidade",
         type: "front"
     },
     {
-        link:"Login-Page",
-        image:"loginpage",
-        alt: "Tela de login e cadastro",
-        type: "front"
-    },
-    {
-        link:"Fitness",
-        image:"fitnesspage",
+        link: "Fitness",
+        image: "fitnesspage",
         alt: "site de academia",
         type: "front"
     },
     {
-        link:"Jazz-School",
-        image:"jazz",
+        link: "Jazz-School",
+        image: "jazz",
         alt: "Site de escola de jazz",
         type: "front"
     },
     {
-        link:"Flower-Delivery",
-        image:"flower",
+        link: "Flower-Delivery",
+        image: "flower",
         alt: "Catalógo de flores",
         type: "front"
     },
     {
-        link:"Pet-Life",
-        image:"petlife",
-        alt: "site para veterinário",
-        type: "front"
-    }
- ]
-
- const dataskills = [
+        link: "tracking-tasks",
+        image: "tracking",
+        alt: "Acompanhador de tarefas",
+        type: "front-vue"
+    },
     {
-        name:"jsimage",
+        link: "cookingup",
+        image: "cooking",
+        alt: "Indicador de receitas",
+        type: "front-vue"
+    },
+    {
+        link: "todolist",
+        image: "TodoList",
+        alt: "Uma Api de registro de atividades em .Net",
+        type: "c#"
+    },
+    {
+        link: "JaFuiV2",
+        image: "jafuiapi",
+        alt: "Uma api para registro de viagens",
+        type: "c#"
+    },
+    {
+        link: "shipping-orders",
+        image: "shipping-orders",
+        alt: "Api para registro de envio de pedidos",
+        type: "c#"
+    }, 
+    {
+        link: "UbBarberShopManager",
+        image: "barberApi",
+        alt: "Api para gerenciamento de barbearia",
+        type: "c#"
+    },
+    {
+        link:"Fina",
+        image:"finacore",
+        alt: "Api para controle financeiro",
+        type: "c#"
+    },
+
+]
+
+const dataskills = [
+    {
+        name: "jsimage",
         alt: "ícone js"
     },
     {
@@ -51,8 +82,8 @@
         alt: "ícone html"
     },
     {
-        name:"tsimage",
-        alt:"ícone type script"
+        name: "tsimage",
+        alt: "ícone type script"
     },
     {
         name: "c-sharp_6132221",
@@ -75,25 +106,34 @@
         alt: "ícone git"
     }
 
- ]
+]
 
- const wrappercards = document.querySelector(".wrapper_projects");
 
- dataProjects.forEach((p) => {
-     wrappercards.insertAdjacentHTML("beforeend", `            <article class="card_project">
+
+const wrappercards = document.querySelector(".wrapper_projects");
+
+dataProjects.forEach((p) => {
+    let completLink = ""
+    if (p.type === "front") {
+        completLink = `https://danielraoli.github.io/${p.link}`
+    } else {
+        completLink = `https://${p.link}.netlify.app/`
+    }
+    wrappercards.insertAdjacentHTML("beforeend", `            <article class="card_project">
                 
      <img src="Assets/${p.image}.png" alt="${p.alt}">
      
      <div class="actions">
          <a href="https://github.com/danielRaoli/${p.link}" target="_blank" class="link_project">
              <img src="Assets/github_733553.png" alt="ícone github">
-         </a>
-         <a href=" https://danielraoli.github.io/${p.link}" target="_blank" class="link_project">
+         </a> 
+         <a href="${completLink}" target="_blank" class="link_project">
              <img src="Assets/startup_5040561.png" alt="ícone de deploy">
          </a>
      </div>
  </article>`)
- });
+});
+
 
 
 const btnAbout = document.querySelector('#btn-about');
@@ -103,9 +143,9 @@ const aboutArea = document.querySelector('#about-area')
 let tecActivate = false;
 let aboutActivate = true;
 
-btnTec.addEventListener('click',() =>{
+btnTec.addEventListener('click', () => {
 
-    if(tecActivate){
+    if (tecActivate) {
         return;
     }
 
@@ -118,42 +158,46 @@ btnTec.addEventListener('click',() =>{
     <div class="skill">
         <img src="Assets/${skill.name}.png" alt="${skill.alt}">
     </div>`)
-    aboutArea.insertAdjacentElement('afterbegin', wraperSkills);
-    const skills = document.querySelectorAll('.skill');
-    
-    skills.forEach((skill) => {
-        setTimeout(() => {
-            skill.classList.add('-activate')
-        },100)
-    })
-    tecActivate = true;
-    aboutActivate = false;
+        aboutArea.insertAdjacentElement('afterbegin', wraperSkills);
+        const skills = document.querySelectorAll('.skill');
+
+        skills.forEach((skill) => {
+            setTimeout(() => {
+                skill.classList.add('-activate')
+            }, 100)
+        })
+        tecActivate = true;
+        aboutActivate = false;
 
     });
-} );
+});
 
 
- btnAbout.addEventListener('click',() => {
-    if(aboutActivate){
+
+
+btnAbout.addEventListener('click', () => {
+    if (aboutActivate) {
         return;
     }
     aboutArea.removeChild(aboutArea.firstChild);
 
-    aboutArea.insertAdjacentHTML('afterbegin',`<p class="description">
+    aboutArea.insertAdjacentHTML('afterbegin', `<p class="description">
     Sou graduado em Análise e Desenvolvimento de Sistemas e sou verdadeiramente apaixonado pelo mundo do desenvolvimento de softwares. Tenho uma habilidade inata para criar soluções eficientes para problemas, independentemente da tecnologia envolvida. Minhas principais habilidades incluem o desenvolvimento back-end utilizando .Net e C#, enquanto no front-end, minha preferência recai sobre o Angular.
     Ao longo da minha jornada profissional, adquiri experiência como freelancer, desenvolvendo aplicações e sites para a web. Minha versatilidade me permite atuar tanto na concepção e implementação de soluções back-end quanto na criação de interfaces front-end de projetos.
     Estou constantemente atualizando meus conhecimentos para acompanhar as inovações do setor, e estou pronto para contribuir de maneira proativa em projetos desafiadores.
      </p>`);
 
-aboutActivate = true;
-tecActivate = false;
+    aboutActivate = true;
+    tecActivate = false;
 
 });
 
-if(aboutActivate){ 
-    aboutArea.insertAdjacentHTML('afterbegin',`<p class="description">
+if (aboutActivate) {
+    aboutArea.insertAdjacentHTML('afterbegin', `<p class="description">
     Sou graduado em Análise e Desenvolvimento de Sistemas e sou verdadeiramente apaixonado pelo mundo do desenvolvimento de softwares. Tenho uma habilidade inata para criar soluções eficientes para problemas, independentemente da tecnologia envolvida. Minhas principais habilidades incluem o desenvolvimento back-end utilizando .Net e C#, enquanto no front-end, minha preferência recai sobre o Angular.
     Ao longo da minha jornada profissional, adquiri experiência como freelancer, desenvolvendo aplicações e sites para a web. Minha versatilidade me permite atuar tanto na concepção e implementação de soluções back-end quanto na criação de interfaces front-end de projetos.
     Estou constantemente atualizando meus conhecimentos para acompanhar as inovações do setor, e estou pronto para contribuir de maneira proativa em projetos desafiadores.
             </p>`)
-}  
+}
+
+
